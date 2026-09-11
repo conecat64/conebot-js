41dc683fae36843f314bf5397b5f5f2c1be9dea1
conecat <conecat100@gmail.com>
2026-09-11 22:26:01 +0200

    bruh

--- utils/errorEmbed.js
+++ utils/errorEmbed.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder, MessageFlags, embedLength } = require('discord.js');
 
-module.exports = async function (interaction, description, makePublic) {
+module.exports = async function (interaction, description, makePublic, error) {
     if (!interaction) return;
     
     let errorEmbed = new EmbedBuilder()
@@ -8,6 +8,8 @@ module.exports = async function (interaction, description, makePublic) {
         .setTitle('Error!')
         .setDescription('`' + description + '`')
         .setTimestamp()
+
+    if (description.toString().indexOf("DiscordAPIError[10062]") != -1) return;
 
     await interaction.reply({
         embeds: [errorEmbed],
